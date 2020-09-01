abcaeb3ec02ff70f0050431898a5149e8ca82d1f
David Ramalho <david.m.ramalho@gmail.com>
2020-09-01 00:22:10 +0000

    created submission form and a response to a post request

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
+function generateRandomString() {
+
+}
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -13,6 +19,10 @@ app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase }; 
   res.render('urls_index', templateVars);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
@@ -27,17 +37,9 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req,res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n')
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`); // THIS WILL NOT RESULT IN a = undefined!!! IT WILL RESPOND WITH A REFERENCE ERROR!
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
